d213e73b994bb2824691cb0361f98cec6657c2a2
sewer56lol <sewer56lol@googlemail.com>
2017-09-01 23:11:08 +0100

    Update | Improve Efficiency and provide a Better Example

--- Heroes-Sample-Mod/Program.cs
+++ Heroes-Sample-Mod/Program.cs
@@ -48,10 +48,22 @@ namespace TestLibrary
                 Sonic_Heroes_Process = Process.GetCurrentProcess(); /// We will use this for reading and writing memory.
                 ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
-                // Time for some hooking, we are hooking with an "Injection Hook" onto the subroutine responsible for rendering a frame to the screen. i.e. This will be ran every frame.
-                Direct2D_Drawing_Injection_Hook = new SonicHeroes.Hooking.Injection((IntPtr)SonicHeroes_Functions.Render_A_Frame.Render_Frame_Subroutine_Start, (Direct2D_Drawing_Injection_Delegate)Run_Subscribed_Method_Direct2D_Injection, 5, Sonic_Heroes_Networking_Client);
-                Direct2D_Drawing_Injection_Hook.Subscribe(Sonic_Heroes_Networking_Client, (Injection_Hook_Sample_Delegate)Direct2D_Hook_Sample); // Subscribe to the methods to be ran on the hooked address.
-                Direct2D_Drawing_Injection_Hook.Activate(); // Redirect the flow to our own game hook here.
+                // Thread used to update the overlay.
+                Heroes_D2D_Update_Thread = 
+                new Thread
+                (
+                    ()=>
+                    {
+                        while (true)
+                        {
+                            Direct2D_Hook_Sample();
+                            Thread.Sleep(16); // Roughly 60FPS
+                        }
+                    }    
+                );
+
+                // Start Direct2D Update Thread
+                Heroes_D2D_Update_Thread.Start();
 
                 // Set up DirectX Rendering.
                 Sonic_Heroes_Overlay.Initialize_DirectX(); // Sets up a rendering device for DirectX
@@ -66,19 +78,10 @@ namespace TestLibrary
         }
 
         /// Own Variables
-        static SonicHeroes.Hooking.Injection Direct2D_Drawing_Injection_Hook; // Hook Type: Injection.
-        delegate void Injection_Hook_Sample_Delegate(); // Must create a delegate where program flow will be redirected to for each hook. Delegate return type and parameters must match method to be executed.
         delegate void Direct2D_Drawing_Injection_Delegate(); // Delegate to handle the subscribe function for the D2D hook.
         static SonicHeroes.Overlay.SonicHeroes_Overlay Sonic_Heroes_Overlay = new SonicHeroes.Overlay.SonicHeroes_Overlay(); // Declare an instance of the Sonic Heroes overlay.
-
-        /// <summary>
-        /// Runs the Subscribed Update Direct2D Overlay Method, this will also automatically run other methods subscribed to this address!
-        /// </summary>
-        static void Run_Subscribed_Method_Direct2D_Injection()
-        {
-            Sonic_Heroes_Networking_Client.SendData(Message_Type.Client_Call_Call_Subscribed_Function, BitConverter.GetBytes((int)Direct2D_Drawing_Injection_Hook.HookAddress), false); 
-            return;
-        }
+        static Thread Heroes_D2D_Update_Thread; // Thread which will process updates.
+        static bool Screen_Clear = false;
 
         /// <summary>
         /// Sample D2D hook code which is injected and ran on each frame.
@@ -89,10 +92,15 @@ namespace TestLibrary
             byte Is_Pause_Menu_Open = HeroesProcess.ReadMemory<byte>(Sonic_Heroes_Process, (IntPtr)SonicHeroesVariables.Game_CurrentState.IsIngamePauseMenuOpen, 1);
 
             // If the pause menu is not open and the player is in a stage.
-            if ((Is_Currently_In_Level == 1) && (Is_Pause_Menu_Open == 0))  { Sonic_Heroes_Overlay.DirectX_Render(); } // Draw Sonic Statistics
-            else { Sonic_Heroes_Overlay.DirectX_Clear_Screen(); } // Render Nothing
+            if ((Is_Currently_In_Level == 1) && (Is_Pause_Menu_Open == 0))
+            {
+                Sonic_Heroes_Overlay.DirectX_Render(); Screen_Clear = false; // Draw Sonic Statistics
+            } 
+            else if (Screen_Clear == false)
+            {
+                Sonic_Heroes_Overlay.DirectX_Clear_Screen(); Screen_Clear = true; // Clear Screen
+            } 
             return;
-
         }
 
         /// Defines the one time calculated rectangle draw properties
@@ -154,17 +162,17 @@ namespace TestLibrary
                 for (int x = 0; x < Drawing_Properties.Sonic_Statistics.Length; x++)
                 {
                     // Text to Draw
-                    DirectX_Graphics_Device.DrawText(Drawing_Properties.Sonic_Statistics[x], Drawing_Properties.Text_Font_DirectX, 
+                    DirectX_Graphics_Device.DrawText(Drawing_Properties.Sonic_Statistics[x], Drawing_Properties.Text_Font_DirectX,
 
                     // Rectangle for Text Wrapping
                     new SharpDX.Mathematics.Interop.RawRectangleF
                     (
                         (int)Drawing_Properties.Rectangle_Menu_DirectX.Left + (int)Drawing_Properties.Line_Spacing, // Left Edge | Make Space Equal to Line Spacing
-                        (int)Drawing_Properties.Rectangle_Menu_DirectX.Top + ( (int)Drawing_Properties.Line_Spacing * x ) + ((int)Drawing_Properties.Line_Height * x), // Top Edge 
+                        (int)Drawing_Properties.Rectangle_Menu_DirectX.Top + ((int)Drawing_Properties.Line_Spacing * x) + ((int)Drawing_Properties.Line_Height * x), // Top Edge 
                         float.PositiveInfinity, // Right Edge | No Text Wrap
                         float.PositiveInfinity // Bottom Edge | No Text Wrap
                     ),
-                    
+
                     // Brush to use
                     Drawing_Properties.Drawing_Brush_DirectX);
                 }
@@ -227,7 +235,8 @@ namespace TestLibrary
 
             // Obtain TextLayout in order to obtain text width and height properties.
             SharpDX.DirectWrite.TextLayout[] All_Text_Layouts = new SharpDX.DirectWrite.TextLayout[Menu_Text_Array.Length]; // Get all text layout info
-            for (int x = 0; x < Menu_Text_Array.Length; x++) {
+            for (int x = 0; x < Menu_Text_Array.Length; x++)
+            {
                 All_Text_Layouts[x] = new TextLayout(new SharpDX.DirectWrite.Factory(), Menu_Text_Array[x], Drawing_Properties.Text_Font_DirectX, float.PositiveInfinity, float.PositiveInfinity);
             }
 
@@ -237,7 +246,8 @@ namespace TestLibrary
 
             // Obtain Largest Width and Height
             float Rectangle_Background_Width = All_Text_Layouts[0].Metrics.Width;
-            for (int x = 1; x < All_Text_Layouts.Length; x++) {
+            for (int x = 1; x < All_Text_Layouts.Length; x++)
+            {
                 if (All_Text_Layouts[x].Metrics.Width > Rectangle_Background_Width) { Rectangle_Background_Width = All_Text_Layouts[x].Metrics.Width; }
             }
 
@@ -249,19 +259,19 @@ namespace TestLibrary
             Rectangle_Background_Width += (Drawing_Properties.Line_Spacing * 2);
 
             // Obtain X and Y position across the form for the selected location of item.
-            int Window_Position_X_Local = (int)( ((Sonic_Heroes_Overlay.OverlayForm.Width / 100.0F) * Window_Position_X));
-            int Window_Position_Y_Local = (int)( ((Sonic_Heroes_Overlay.OverlayForm.Height / 100.0F) * Window_Position_Y));
+            int Window_Position_X_Local = (int)(((Sonic_Heroes_Overlay.OverlayForm.Width / 100.0F) * Window_Position_X));
+            int Window_Position_Y_Local = (int)(((Sonic_Heroes_Overlay.OverlayForm.Height / 100.0F) * Window_Position_Y));
 
             // Get center location of Rectangle & Size
             // X is left edge. 
             // Y is right edge.
-            Point Rectangle_Location = new Point(Window_Position_X_Local - (int)(Rectangle_Background_Width / 2.0F), Window_Position_Y_Local - (int)(Rectangle_Background_Height / 2.0F) );
+            Point Rectangle_Location = new Point(Window_Position_X_Local - (int)(Rectangle_Background_Width / 2.0F), Window_Position_Y_Local - (int)(Rectangle_Background_Height / 2.0F));
             Size Rectangle_Size = new Size((int)(Rectangle_Background_Width), (int)(Rectangle_Background_Height));
 
             // Ensure Rectangle doesn't escape screen space.
 
             // If the right edge will escape the screen, make sure it does not.
-            if ( (Rectangle_Size.Width + Rectangle_Location.X) > Sonic_Heroes_Overlay.OverlayForm.Width)
+            if ((Rectangle_Size.Width + Rectangle_Location.X) > Sonic_Heroes_Overlay.OverlayForm.Width)
             {
                 int Menu_Overflow_Pixels = (Rectangle_Size.Width + Rectangle_Location.X) - Sonic_Heroes_Overlay.OverlayForm.Width; // Get the amount of pixels rectangle escapes of screen.
                 Rectangle_Location.X = Rectangle_Location.X - Menu_Overflow_Pixels; // Move the rectangle by the overflow amount.
