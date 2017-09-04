b86ee0de3a6dc614473ed9f6169950e609299c92
sewer56lol <sewer56lol@googlemail.com>
2017-09-04 18:10:20 +0100

    Upload

---
+++ Heroes-Sample-Mod/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using SonicHeroes.Functions;
+using SonicHeroes.Controller;
+using SonicHeroes.Memory;
+using SonicHeroes.Variables;
+using System.Diagnostics;
+using System.Net;
+using System.Windows.Forms;
+using System.Text;
+using static SonicHeroes.Networking.Client_Functions;
+using System.Drawing;
+using SonicHeroes.Overlay;
+using SharpDX;
+using SharpDX.Direct2D1;
+using SharpDX.Mathematics.Interop;
+using SharpDX.DirectWrite;
+using Heroes_Sample_Mod;
+using System.Threading;
+
+namespace TestLibrary
+{
+    public class Program
+    {
+        /// Mod loader DLL Skeleton Code
+        const string Mod_Name = "Mod Loader Magnetic Barrier Sample"; // Set name of project.
+        static SonicHeroes.Networking.WebSocket_Client Sonic_Heroes_Networking_Client = new SonicHeroes.Networking.WebSocket_Client(); // Set up client for networking, this client communicates with the server to call methods under subscribe hook
+        static SonicHeroes.Networking.WebSocket_Client Sonic_Heroes_Networking_Client_II = new SonicHeroes.Networking.WebSocket_Client(); // Set up client for networking, this one is for non-subscribed calls.
+        static Process Sonic_Heroes_Process; // Get Sonic Heroes Process
+        /// Mod loader DLL Skeleton Code
+
+        /// <summary>
+        /// Your program starts here!
+        /// </summary>
+        [DllExport]
+        static void Main()
+        {
+            try
+            {
+                ////////////// MOD LOADER DLL SKELETON CODE ///////////////////////////////////////////////////////////////////////////////////////////////////
+                AppDomain.CurrentDomain.AssemblyResolve += new ResolveEventHandler(SonicHeroes.Misc.SonicHeroes_Miscallenous.CurrentDomain_SetAssemblyResolve);
+                Sonic_Heroes_Networking_Client.SetupClient(IPAddress.Loopback); /// Set up networking with the Sonic Heroes Mod Loader.
+                Sonic_Heroes_Networking_Client_II.SetupClient(IPAddress.Loopback); /// Set up networking with the Sonic Heroes Mod Loader.
+                byte[] Response = Sonic_Heroes_Networking_Client.SendData_Alternate(Message_Type.Client_Call_Send_Message, Encoding.ASCII.GetBytes(Mod_Name + " | Loading... OK!"), true); /// Say to the Mod Loader that we have loaded so the end user can know.
+                Sonic_Heroes_Process = Process.GetCurrentProcess(); /// We will use this for reading and writing memory.
+                ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+                // Your code starts here ;)
+
+            }
+            catch (Exception Ex) { Sonic_Heroes_Networking_Client.SendData_Alternate(Message_Type.Client_Call_Send_Message, Encoding.ASCII.GetBytes(Mod_Name + " Failed To Load: " + Ex.Message), false); }
+        }
+    }
+}
