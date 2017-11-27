da4adc4e9179c17f8f015edcc430c639c759723d
sewer56lol <sewer56lol@googlemail.com>
2017-11-27 00:48:45 +0000

    Temporary File Unblocker

---
+++ Heroes-Mod-Loader/FileUnblocker.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HeroesInjectionTest
+{
+    public class FileUnblocker
+    {
+        [DllImport("kernel32", CharSet = CharSet.Unicode, SetLastError = true)]
+        [return: MarshalAs(UnmanagedType.Bool)]
+        private static extern bool DeleteFile(string name);
+
+        public bool Unblock(string fileName)
+        {
+            return DeleteFile(fileName + ":Zone.Identifier");
+        }
+    }
+}
